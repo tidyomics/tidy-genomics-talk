cc1e861fa697da72a31bea96dc7ce83c363e09ea
mikelove <michaelisaiahlove@gmail.com>
2022-12-11 09:08:57 -0500

    add enrichment slides

--- boot_and_match_examples.R
+++ boot_and_match_examples.R
@@ -37,14 +37,15 @@ plotSomeGenes(chrom, rng, showGuides=FALSE)
 
 # make n features in clumps of ~lambda
 seqlens <- seqlengths(g)[chrom]
-gr <- makeClusterRanges(chrom, rng_big, n=300, lambda=5, seqlens)
+set.seed(5)
+p <- makeClusterRanges(chrom, rng_big, n=300, lambda=5, seqlens)
 
 # define some plotting parameters for plotgardener,
 # e.g. a palette for feature 'score':
 pal <- colorRampPalette(c("dodgerblue2", "firebrick2"))
 
 # shared genomic location, width & height, x position, fill, etc.
-p <- pgParams(
+params <- pgParams(
   chrom=chrom, chromstart=rng[1], chromend=rng[2],
   width=5.5, height=1, x=.25,
   fill=colorby("score", palette=pal),
@@ -52,29 +53,30 @@ p <- pgParams(
 )
 
 # shared parameters for text labels
-textp <- pgParams(x=.1, rot=90, just="left")
+textparams <- pgParams(x=.1, rot=90, just="left")
 
-# plot the original GRanges
-plotRanges(gr, params=p, y=2)
-plotText("original", params=textp, y=3)
+# plot the original GRanges, e.g. suppose ATAC-seq peaks
+plotRanges(p, params=params, y=2)
+plotText("original", params=textparams, y=3)
 
 # uniform shuffling
-shuf <- shuffle(gr, rng_big)
+shuf <- shuffle(p, rng_big)
 
 # plot shuffled ranges
-plotRanges(shuf, params=p, y=1)
-plotText("shuffled", params=textp, y=2)
+plotRanges(shuf, params=params, y=1)
+plotText("shuffled", params=textparams, y=2)
 
 # segmented block bootstrapping
 # blocks 100kb, not proportion to segment length
 library(nullranges)
 seg <- makeSegmentation(chrom, rng_big, seqlens)
-boot <- bootRanges(gr, blockLength=1e5, R=1,
+set.seed(1)
+boot <- bootRanges(p, blockLength=1e5, R=1,
                    seg=seg, proportionLength=FALSE)
 
 # plot bootstrapped ranges
-plotRanges(boot, params=p, y=0)
-plotText("boot", params=textp, y=1)
+plotRanges(boot, params=params, y=0)
+plotText("boot", params=textparams, y=1)
 
 # for genome-wide analysis, consider excluding gaps, repeats, etc.
 # see https://dozmorovlab.github.io/excluderanges for details
@@ -90,16 +92,18 @@ plotText("boot", params=textp, y=1)
 # first just counts as statistic
 
 g %>%
-  mutate(n_overlaps = count_overlaps(., gr))
+  mutate(n_overlaps = count_overlaps(., p))
 
-g %>% join_overlap_left(gr) %>%
+g %>% 
+  join_overlap_left(p) %>%
   group_by(symbol) %>%
   summarize(n_overlaps = sum(!is.na(id)))
 
 # working with metadata
 
-g %>% join_overlap_left(gr) %>%
-  group_by(symbol) %>%
+g %>% 
+  join_overlap_left(p) %>%
+  group_by(symbol) %>% # per gene symbol
   summarize(sum_score = sum(score))
 
 # simple violin plot
@@ -107,7 +111,8 @@ g %>% join_overlap_left(gr) %>%
 library(tibble)
 library(ggplot2)
 # inner instead of left: leaves out no-overlap genes
-g %>% join_overlap_inner(gr) %>%
+g %>% 
+  join_overlap_inner(p) %>%
   mutate(type = "original") %>%
   group_by(symbol, type) %>%
   summarize(sum_score = sum(score)) %>%
@@ -124,7 +129,8 @@ sim_list <- replicate(niter, {
 })
 sim_long <- bind_ranges(sim_list, .id="iter")
 
-g %>% join_overlap_inner(sim_long) %>%
+g %>% 
+  join_overlap_inner(sim_long) %>%
   mutate(type = "original") %>%
   group_by(symbol, iter, type) %>%
   summarize(sum_score = sum(score)) %>%
@@ -135,10 +141,10 @@ g %>% join_overlap_inner(sim_long) %>%
 
 # shuffling and bootstrapping multiple times
 
-shuf_list <- replicate(niter, shuffle(gr, rng_big))
+shuf_list <- replicate(niter, shuffle(p, rng_big))
 shuf_long <- bind_ranges(shuf_list, .id="iter")
 
-boot_long <- bootRanges(gr, blockLength=1e5, R=niter,
+boot_long <- bootRanges(p, blockLength=1e5, R=niter,
                    seg=seg, proportionLength=FALSE)
 
 # bind together
@@ -154,7 +160,8 @@ head(table(all$iter, all$type))
 # final plot of distributions:
 # multiple draws, shuffling one instance, bootstrapping one instances
 
-g %>% join_overlap_inner(all) %>%
+g %>% 
+  join_overlap_inner(all) %>%
   group_by(symbol, iter, type) %>%
   summarize(sum_score = sum(score)) %>%
   as_tibble() %>%
@@ -171,13 +178,14 @@ plotSomeGenes(chrom, rng, showGuides=FALSE)
 
 # make some features with particular distribution
 # 1) near gene TSS, 2) tend to have large 'score' values
+set.seed(1)
 focal <- makeFocalFeatures(g, chrom, rng)
 
 # 5 color palette for 'score'
 pal <- colorRampPalette(c("blue","green","yellow","red"))
 
 # new plot parameters
-p <- pgParams(
+params <- pgParams(
   chrom=chrom, chromstart=rng[1], chromend=rng[2],
   width=5.5, height=1, x=.25,
   fill=colorby("score", palette=pal, range=c(1,5)),
@@ -185,15 +193,15 @@ p <- pgParams(
 )
 
 # plot the original 'focal' GRanges
-plotRanges(focal, params=p, y=2)
-plotText("focal", params=textp, y=3)
+plotRanges(focal, params=params, y=2)
+plotText("focal", params=textparams, y=3)
 
 # make a 'pool' of features to select from
 pool <- makePool(5000, chrom, rng, seqlens)
 
 # plot the pool (subset)
-plotRanges(pool[1:200], params=p, y=1)
-plotText("pool", params=textp, y=2)
+plotRanges(pool[1:200], params=params, y=1)
+plotText("pool", params=textparams, y=2)
 
 # add another feature: distance to nearest TSS
 tss <- g %>% anchor_5p() %>% mutate(width=1)
@@ -204,20 +212,23 @@ both <- bind_ranges(focal = focal, pool = pool, .id="type") %>%
 
 hist(both$log10dist)
 
-# three different methods for matching
-m <- matchRanges(both[both$type == "focal"],
-                 both[both$type == "pool"],
-                 covar=~score + log10dist,
-                 method="nearest", replace=TRUE)
+m <- both %>% {
+  matchRanges(filter(., type=="focal"),
+              filter(., type=="pool"),
+              covar=~score + log10dist,
+              method="nearest", replace=TRUE)
+}
 
-plotCovariate(m, covar="score")
-plotCovariate(m, covar="log10dist")
+library(patchwork)
+plotCovariate(m, covar="score") +
+  plotCovariate(m, covar="log10dist")
 
 # plot the matched set (need to replot the others)
+set.seed(1)
 plotSomeGenes(chrom, rng, showGuides=FALSE)
-plotRanges(focal, params=p, y=2)
-plotText("focal", params=textp, y=3)
-plotRanges(pool[1:200], params=p, y=1)
-plotText("pool", params=textp, y=2)
-plotRanges(matched(m), params=p, y=0)
-plotText("matched", params=textp, y=1)
+plotRanges(focal, params=params, y=2)
+plotText("focal", params=textparams, y=3)
+plotRanges(pool[1:200], params=params, y=1)
+plotText("pool", params=textparams, y=2)
+plotRanges(matched(m), params=params, y=0)
+plotText("matched", params=textparams, y=1)
